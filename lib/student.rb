5f9a0387392cd5ad260ca18ee76b2d0f490abdbc
Rebecca Congi <rncongi@gmail.com>
2020-08-27 00:37:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,15 +1,13 @@
 class Student < User 
   
+  attr_reader :knowledge
+  
   def initialize
     @knowledge = []
   end   
   
   def learn(lesson)
     @knowledge << lesson 
-  end   
-  
-  def knowledge
-    @knowledge
   end   
   
 end
