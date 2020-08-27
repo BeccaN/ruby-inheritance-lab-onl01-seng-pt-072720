7670119e3a80d8a4b2abbf8d3071421abe43b966
Rebecca Congi <rncongi@gmail.com>
2020-08-27 00:08:28 +0000

    Automatically backed up by Learn

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User 
+  
+  attr_accessor :first_name, :last_name
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @second_name = second_name
+  end
+  
+end   
